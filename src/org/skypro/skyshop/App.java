12ebd1a63b1eb50f42fef503bd6296e19b90515d
Алексей Дорофеев <aleksey.dorofeev@rmg-region.ru>
2025-02-04 20:27:54 +0300

    Attempt 1 Добавлены методы:  - Добавление продукта в корзину  - Печать содержимого корзины  - Получение стоимости корзины  - Поиск товар в корзине  - Очистка корзины

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -1,8 +1,41 @@
 package org.skypro.skyshop;
 
+import org.skypro.skyshop.basket.ProductBasket;
+
 public class App {
 
     public static void main(String[] args) {
+        ProductBasket productBasket = new ProductBasket(5);
+        System.out.println("Добавление продуктов ");
+        productBasket.addProduct(new Product("Рыба", 50));
+        productBasket.addProduct(new Product("Мясо", 100));
+        productBasket.addProduct(new Product("Сыр", 30));
+        productBasket.addProduct(new Product("Конфеты", 50));
+        productBasket.addProduct(new Product("Лук", 200));
+        System.out.println("Добавление продуктов - нет места");
+        productBasket.addProduct(new Product("Лук", 200));
+        System.out.println("____________________________________");
+        System.out.println("Печать корзины");
+        productBasket.printMyBasket();
+        System.out.println("____________________________________");
+        System.out.println("Стоимость корзины");
+        int a = productBasket.getAllPrice();
+        System.out.println(a);
+        System.out.println("____________________________________");
+        System.out.println("Поиск товара - есть");
+        productBasket.isNoProductInBasket("Рыба");
+        System.out.println("____________________________________");
+        System.out.println("Поиск товара - нет");
+        productBasket.isNoProductInBasket("Пицуня");
+        System.out.println("____________________________________");
+        System.out.println("Обнулить корзину");
+        productBasket.clearBasket();
+        System.out.println("____________________________________");
+        System.out.println("Печать пустой корзины");
+        productBasket.printMyBasket();
+        System.out.println("____________________________________");
+        System.out.println("Стоимость пустой корзины");
+        System.out.println(productBasket.getAllPrice());
 
     }
 }
